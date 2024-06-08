52198d5b9e456862aa8c63aa7673a8629b58d22d
SayidanNazmyd <nazmydsayidan@gmail.com>
2024-06-08 13:18:45 +0700

    gaada

---
+++ resources/views/pages/dashboard/user/create.blade.php
@@ -0,0 +1,62 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Pruduct &raquo; Create
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div>
+                @if ($errors->any())
+                    <div class="mb-5" role="alert">
+                        <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                            There's something wrong!!!
+                        </div>
+                        <div class="border border-t-0 border-red-400 rounded-b bg-red-100 py-4 px-3 text-red-700">
+                            <p>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                @endif
+                <form action="{{route('dashboard.product.store')}}" class="w-full" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block upppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Name</label>
+                            <input type="text" name="name" value="{{old('name')}}" placeholder="Name Product" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="">
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block upppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Description</label>
+                            <textarea name="description" id="description" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="">{!! old('description') !!}</textarea>
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block upppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Price</label>
+                            <input type="number" name="price" value="{{old('price')}}" placeholder="Product price" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="">
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
+                                save product
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.ckeditor.com/ckeditor5/41.4.2/classic/ckeditor.js"></script>
+    <script>
+        ClassicEditor
+                .create( document.querySelector( '#description' ) )
+    </script>
+</x-app-layout>
